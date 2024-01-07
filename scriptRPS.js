5d541247f78d36545b1f60043e9c72cf3e109fe5
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-01-06 18:06:13 -0600

    Fixed functionality of game functionality
    Correct incrementing of scores

--- scriptRPS.js
+++ scriptRPS.js
@@ -1,13 +1,13 @@
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
         case 0:
-            console.log("Rock");
+            // console.log("Rock");
             return "Rock";
         case 1:
-            console.log("Paper");
+            // console.log("Paper");
             return "Paper";
         case 2:
-            console.log("Scissors");
+            // console.log("Scissors");
             return "Scissors";
     }
 }
@@ -22,38 +22,35 @@ function playRound(playerSelection, computerSelection) {
 
     if (player == comp) {
         alert("Uh oh...you made the same choice as the computer. No one wins. You must play again.")
-        playRound(playerSelection=prompt("Best of 5. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice());
+        playRound(playerSelection = prompt("Best of 5. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice());
     }
 
     if ((player == "ROCK" && comp == "SCISSORS") || (player == "SCISSORS" && comp == "PAPER") || (player == "PAPER" && comp == "ROCK")) {
         return `You win! ${player} beats ${comp}!`
-    } 
-    else {return `You lose! ${comp} beats ${player}.`} 
+    }
+    else { return `You lose! ${comp} beats ${player}.` }
 }
 
-// console.log(playRound(playerSelection=prompt("Best of 5. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice()))
 
+function game() {
+    let playerScore = 0;
+    let compScore = 0;
+    while (playerScore < 3 && compScore < 3) {
 
+        result = playRound(playerSelection = prompt("This game is best of 5 so first to 3 wins. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice())
+        console.log(result);
 
-function game(){
-    let playerScore=0;
-    let compScore=0;
-while (playerScore<3 && compScore<3) {
+        if (result.substr(4, 1) == "w") {
+            console.log(`Your score is ${++playerScore}. The computer's score is ${compScore}.`);
+        } else {
+            console.log(`Your score is ${playerScore}. The computer's score is ${++compScore}.`);
+        }
 
-result=playRound(playerSelection=prompt("This game is best of 5 so first to 3 wins. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice())
-console.log(result);
+    }
 
-if (result.substr(4,1)=="w") {
-    playerScore=playerScore++;
-} else {compScore=compScore++;}
-
-console.log(`Your score is ${playerScore}. The computer's score is ${compScore}.`);
-
-}
-
-if (playerScore==3) {
-    alert("You won 3 of the 5 games. You are the ultimate, grand daddy, master, champion of rock, paper, scissors. Congrats!")
-} else {alert("You lost 3 of the 5 games...to a computer... :(")}
+    if (playerScore == 3) {
+        alert("You won 3 of the 5 games. You are the ultimate, grand daddy, master, champion of rock, paper, scissors. Congrats!")
+    } else { alert("You lost 3 of the 5 games...to a computer... :(") }
 
 }
 
