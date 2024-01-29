620ae47ef4d24ca411de03f29c0a10e5eb9219cb
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-01-29 17:54:43 -0600

    Removed logic that plays exactly 5 rounds
    Created 3 buttons

--- scriptRPS.js
+++ scriptRPS.js
@@ -28,32 +28,37 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if ((player == "ROCK" && comp == "SCISSORS") || (player == "SCISSORS" && comp == "PAPER") || (player == "PAPER" && comp == "ROCK")) {
+        alert(`You win! ${player} beats ${comp}!`);
         return `You win! ${player} beats ${comp}!`
     }
-    else { return `You lose! ${comp} beats ${player}.` }
+    else { 
+        alert(`You lose! ${comp} beats ${player}.`)
+        return `You lose! ${comp} beats ${player}.` }
 }
 
-// initiate best of 5
-function game() {
-    let playerScore = 0;
-    let compScore = 0;
-    while (playerScore < 3 && compScore < 3) {
+playRound(playerSelection = prompt("Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice())
 
-        result = playRound(playerSelection = prompt("This game is best of 5 so first to 3 wins. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice())
-        console.log(result);
 
-        if (result.substr(4, 1) == "w") {
-            console.log(`Your score is ${++playerScore}. The computer's score is ${compScore}.`);
-        } else {
-            console.log(`Your score is ${playerScore}. The computer's score is ${++compScore}.`);
-        }
+// // initiate best of 5
+// function game() {
+//     let playerScore = 0;
+//     let compScore = 0;
+//     while (playerScore < 3 && compScore < 3) {
 
-    }
+//         result = playRound(playerSelection = prompt("This game is best of 5 so first to 3 wins. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice())
+//         console.log(result);
 
-    if (playerScore == 3) {
-        alert("You won 3 of the 5 games. You are the ultimate, grand daddy, master, champion of rock, paper, scissors. Congrats!")
-    } else { alert("You lost 3 of the 5 games...to a computer... :(") }
+//         if (result.substr(4, 1) == "w") {
+//             console.log(`Your score is ${++playerScore}. The computer's score is ${compScore}.`);
+//         } else {
+//             console.log(`Your score is ${playerScore}. The computer's score is ${++compScore}.`);
+//         }
+//     }
 
-}
+//     if (playerScore == 3) {
+//         alert("You won 3 of the 5 games. You are the ultimate, grand daddy, master, champion of rock, paper, scissors. Congrats!")
+//     } else { alert("You lost 3 of the 5 games...to a computer... :(") }
 
-game()
+// }
+
+// game()
