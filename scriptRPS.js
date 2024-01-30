267e28ed6e72a341eab29dfb3af68e3a569ac392
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-01-29 19:01:03 -0600

    Added event listeners to the buttons

--- scriptRPS.js
+++ scriptRPS.js
@@ -1,57 +1,61 @@
 const rock=document.getElementById("rock");
 const paper=document.getElementById("paper");
 const scissors=document.getElementById("scissors");
+const buttons = document.getElementsByTagName("button");
 
-console.log(rock);
-console.log(paper);
-console.log(scissors);
+// get computer choice
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            // console.log("Rock");
+            return "Rock";
+        case 1:
+            // console.log("Paper");
+            return "Paper";
+        case 2:
+            // console.log("Scissors");
+            return "Scissors";
+    }
+}
 
+// console.log(`The Computer chose: ${getComputerChoice()}`);
 
-// // get computer choice
-// function getComputerChoice() {
-//     switch (Math.floor(Math.random() * 3)) {
-//         case 0:
-//             // console.log("Rock");
-//             return "Rock";
-//         case 1:
-//             // console.log("Paper");
-//             return "Paper";
-//         case 2:
-//             // console.log("Scissors");
-//             return "Scissors";
-//     }
-// }
+// initiate round
 
-// // initiate round
-// function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
 
-//     let player = playerSelection.toUpperCase();
-//     let comp = computerSelection.toUpperCase();
+    let player = playerSelection.toUpperCase();
+    let comp = computerSelection.toUpperCase();
 
-//     console.log(`You chose: ${playerSelection}.`);
-//     console.log(`The computer chose: ${computerSelection}.`);
+    console.log(`You chose: ${playerSelection}.`);
+    console.log(`The computer chose: ${computerSelection}.`);
 
-//     if (player == comp) {
-//         alert("Uh oh...you made the same choice as the computer. No one wins. You must play again.")
-//         // playRound(playerSelection = prompt("Best of 5. Please enter your choice of 'rock', 'paper', or 'scissors'."), getComputerChoice());
-//         rock.addEventListener("click", playRound(playerSelection="rock", getComputerChoice()));
-//         paper.addEventListener("click", playRound(playerSelection="paper", getComputerChoice()));
-//         scissors.addEventListener("click", playRound(playerSelection="scissors", getComputerChoice()));
+    if (player == comp) {
+        alert("Uh oh...you made the same choice as the computer. No one wins. You must play again.")
+        
+        for(let i=0; i<buttons.length; i++) {
+            buttons[i].addEventListener("click", function(){
+                playRound(playerSelection=buttons[i].innerHTML, computerSelection=getComputerChoice());
+            })
+        }
+        
+    }
 
-//     }
+    if ((player == "ROCK" && comp == "SCISSORS") || (player == "SCISSORS" && comp == "PAPER") || (player == "PAPER" && comp == "ROCK")) {
+        alert(`You win! ${player} beats ${comp}!`);
+        return `You win! ${player} beats ${comp}!`
+    }
+    else { 
+        alert(`You lose! ${comp} beats ${player}.`)
+        return `You lose! ${comp} beats ${player}.` }
+}
 
-//     if ((player == "ROCK" && comp == "SCISSORS") || (player == "SCISSORS" && comp == "PAPER") || (player == "PAPER" && comp == "ROCK")) {
-//         alert(`You win! ${player} beats ${comp}!`);
-//         return `You win! ${player} beats ${comp}!`
-//     }
-//     else { 
-//         alert(`You lose! ${comp} beats ${player}.`)
-//         return `You lose! ${comp} beats ${player}.` }
-// }
+for(let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        playRound(playerSelection=buttons[i].innerHTML, computerSelection=getComputerChoice());
+    })
+}
 
-// rock.addEventListener("click", playRound(playerSelection="rock", getComputerChoice()));
-// paper.addEventListener("click", playRound(playerSelection="paper", getComputerChoice()));
-// scissors.addEventListener("click", playRound(playerSelection="scissors", getComputerChoice()));
 
 
 // // initiate best of 5
